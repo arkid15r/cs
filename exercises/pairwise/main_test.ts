eaa1443c70daa7b89a85f56f2392d0149f3bd8df
Kate <kate@kgthreads.com>
2022-03-04 19:25:39 -0800

    Edit tests.

--- exercises/pairwise/main_test.ts
+++ exercises/pairwise/main_test.ts
@@ -9,28 +9,23 @@ const should = _should();
 
 describe('Pairwise', () => {
 	it('should return 0', () => {
-		const result = pairwise([], 100);
-		assert.isTrue(isEqual(result, 0));
+		assert.isTrue(isEqual(pairwise([], 100), 0));
 	});
 
 	it('should return 1', () => {
-		const result = pairwise([1, 3, 2, 4], 4);
-		assert.isTrue(isEqual(result, 1));
+		assert.isTrue(isEqual(pairwise([1, 3, 2, 4], 4), 1));
 	});
 
 	it('should return 1', () => {
-		const result = pairwise([1, 1, 1], 2);
-		assert.isTrue(isEqual(result, 1));
+		assert.isTrue(isEqual(pairwise([1, 1, 1], 2), 1));
 	});
 
 	it('should return 11', () => {
-		const result = pairwise([1, 4, 2, 3, 0, 5], 7);
-		assert.isTrue(isEqual(result, 11));
+		assert.isTrue(isEqual(pairwise([1, 4, 2, 3, 0, 5], 7), 11));
 	});
 
 	it('should return 10', () => {
-		const result = pairwise([0, 0, 0, 0, 1, 1], 1);
-		assert.isTrue(isEqual(result, 10));
+		assert.isTrue(isEqual(pairwise([0, 0, 0, 0, 1, 1], 1), 10));
 	});
 
 	it('should return 20', () => {
